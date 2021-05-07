43d43b40a9ecf590694f752dd120cade7232e844
Alan Berndt <alan@eatabrick.org>
2021-05-06 21:30:14 -0700

    Update gam version and start windowed.

--- WORKSPACE
+++ WORKSPACE
@@ -3,5 +3,5 @@ load("@bazel_tools//tools/build_defs/repo:git.bzl", "git_repository")
 git_repository(
     name = "libgam",
     remote = "https://git.sr.ht/~bentglasstube/gam",
-    commit = "2d0fb8776d4b12dcbe43c238e68f9210a26a88a2",
+    commit = "f99d93cfd6e84fa9269e16a2e80df0d781c8b64e",
 )
--- main.cc
+++ main.cc
@@ -1,12 +1,33 @@
-#include <SDL2/SDL.h>
+#include "game.h"
 
 #include "title_screen.h"
 
-#include "game.h"
+#ifdef __EMSCRIPTEN__
+#include "emscripten.h"
+
+void step(void* game) {
+  static_cast<Game*>(game)->step();
+}
+#endif
 
 int main(int, char**) {
-  Game game("Catapults", 640, 360);
-  game.loop(new TitleScreen());
+
+  Game::Config config;
+
+  config.graphics.title = "Catapults";
+  config.graphics.width = 640;
+  config.graphics.height = 360;
+  config.graphics.fullscreen = false;
+
+  Game game(config);
+  Screen *start = new TitleScreen();
+
+#ifdef __EMSCRIPTEN__
+  game.start(start);
+  emscripten_set_main_loop_arg(step, &game, 0, true);
+#else
+  game.loop(start);
+#endif
 
   return 0;
 }
